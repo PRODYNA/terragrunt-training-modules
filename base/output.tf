f316743c451474b4a3f5bfe13772ac625ef49cdc
Matthias Hauber <matthias.hauber@prodyna.com>
2023-06-01 15:29:53 +0200

    .

--- base/output.tf
+++ base/output.tf
@@ -11,7 +11,7 @@ output "private_dns_zone_id" {
 }
 
 output "pip_ids" {
-  value = azurerm_public_ip.vm[*].id # this prints out a list ["",""] with ids
+  value = values(azurerm_public_ip.vm)[*].id # this prints out a list ["",""] with ids
 }
 
 output "asg_id" {
