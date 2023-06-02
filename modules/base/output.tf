035d331007ea0d05e21d2c7d3f00e66b821a7b7a
Matthias Hauber <matthias.hauber@prodyna.com>
2023-06-02 17:22:19 +0200

    fix pip output

--- modules/base/output.tf
+++ modules/base/output.tf
@@ -11,7 +11,7 @@ output "private_dns_zone_id" {
 }
 
 output "pip_ids" {
-  value = values(azurerm_public_ip.vm)[*].id # this prints out a list ["",""] with ids
+  value =  value = [for v in azurerm_public_ip.vm : v.id] # values(azurerm_public_ip.vm)[*].id # this prints out a list ["",""] with ids
 }
 
 output "asg_id" {
